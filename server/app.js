216f550ae5412ae99e6a4763fb2684d454242b25
C S Jeevan Kumar Vadee <jvadde@innominds.com>
2023-09-14 12:31:01 +0530

    fixed the cors error

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const app = express();
 
 // rest of the packages
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 // database
 const connectDB = require("./db/connect");
@@ -17,6 +18,8 @@ const authRouter = require("./routes/authRoutes");
 // middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
